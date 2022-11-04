5495c870ac5c3fe0647d021bb95e1a98419c65e3
Sam Jensen <samual.jensen@gmail.com>
2022-11-04 14:55:33 -0700

    added code for script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,65 @@
-// Assignment code here
-
+// Variables
+var characterLength = 8;
+var choiceArray = [];
+var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password function
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var correctPrompts = getPrompts(); //returns true or false 
 
-  passwordText.value = password;
-
+  if (correctPrompts) {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
+  else {
+    passwordText.value = "";
+  }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    password += (Math.floor(Math.random() * choiceArray.length));
+    password = password + choiceArray[randomnPassword];
+  }
+  return password;
+}
+
+// Function to generate password prompt
+function getPrompts() {
+
+  var choiceArray = [];
+  var characterLength = parseInt(prompt("Hello there, your new password must be 8-128 characters."));
+
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("ALERT: Password must be between 8 - 128 characters.");
+    return false;
+  }
+
+  if (confirm("Would you like any lower case letters?")) {
+    choiceArray = choiceArray.concat(lowerCase);
+  }
+
+  if (confirm("Would you like any upper case letters?")) {
+    choiceArray = choiceArray.concat(upperCase);
+  }
+
+  if (confirm("Would you like any numbers?")) {
+    choiceArray = choiceArray.concat(number);
+  }
+
+  if (confirm("Would you like any special characters?")) {
+    choiceArray = choiceArray.concat(specialCharacter);
+  }
+  return true;
+}
