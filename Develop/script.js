df59aad05080a0ce36ebaa2917362253e15a1b3e
Sam Jensen <samual.jensen@gmail.com>
2022-11-04 10:14:41 -0700

    added files to repo

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,7 @@
-// Assignment Code
+// Assignment code here
+
+
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,8 +15,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-function generatePassword() {
-  
-}
