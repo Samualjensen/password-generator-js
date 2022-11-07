df93a982fe813573b9abf02338329c6cb96d43a1
Sam Jensen <samual.jensen@gmail.com>
2022-11-06 16:17:15 -0800

    added README.md

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,21 @@
-// Variables
+//global variable for possible password length
 var characterLength;
+//global variable for choice array
 var choiceArray = [];
+//lists possible value for password
 var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// Get references to the #generate element
+//get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password function
+//generates intial password prompt
 function writePassword() {
-  var correctPrompts = getPrompts(); //returns true or false 
+  var correctPrompts = getPrompts(); 
   var passwordText = document.querySelector("#password");
-
+  //if statement to ask to ask if user would like to make a password
   if (correctPrompts) {
     var password = generatePassword();
     passwordText.value = password;
@@ -23,14 +25,15 @@ function writePassword() {
   }
 }
 
-// Add event listener to generate button
+//add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//function to generate password
 function generatePassword() {
   var password = "";
   console.log(choiceArray, "length")
   console.log(characterLength, "character length")
-  
+
   for (var i = 0; i < characterLength; i++) {
     randomIndex = (Math.floor(Math.random() * choiceArray.length));
     console.log(randomIndex, "index")
@@ -40,31 +43,39 @@ function generatePassword() {
   return password;
 }
 
-// Function to generate password prompt
+//function to generate password prompt
 function getPrompts() {
 
-  // var choiceArray = [];
+  //prompt asking for length of password
   characterLength = parseInt(prompt("Hello there, your new password must be 8-128 characters."));
 
+  //alert if you type a number that does not meet critria of prompt
   if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
     alert("ALERT: Password must be between 8 - 128 characters.");
     return false;
   }
 
-  if (confirm("Would you like any lower case letters?")) {
-    choiceArray = choiceArray.concat(lowerCase);
-  }
-
-  if (confirm("Would you like any upper case letters?")) {
-    choiceArray = choiceArray.concat(upperCase);
-  }
-
-  if (confirm("Would you like any numbers?")) {
-    choiceArray = choiceArray.concat(number);
-  }
-
+  //if conirm than will add specialCharcter to choiceArray
   if (confirm("Would you like any special characters?")) {
     choiceArray = choiceArray.concat(specialCharacter);
+
+    //if conirm than will add numbers to choiceArray
+    if (confirm("Would you like any numbers?")) {
+      choiceArray = choiceArray.concat(number);
+
+      //if conirm than will add upperCase to choiceArray
+      if (confirm("Would you like any upper case letters?")) {
+        choiceArray = choiceArray.concat(upperCase);
+
+        //if conirm than will add lowerCase to choiceArray
+        if (confirm("Would you like any lower case letters?")) {
+          choiceArray = choiceArray.concat(lowerCase);
+        }
+
+      }
+
+    }
+
   }
   return true;
 }
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# password-generator-js
+# JavaScript Challenge: Password Generator
+ 
+## Description
+
+The third weekly challenge is to use the starter code provided to us and write the javascript portion of it to make it generate a random password. To meet the goal of the challenge we need to write code that that creates prompts that ask the user to select if they would like to make a password, how many characters that password will be, and then a series of prompts that defines what characters that password will consist of. When the user finishes the series of prompts than it will output a random password based on their choices.
+
+## Links 
+
+Link to live url: https://samualjensen.github.io/class-challenge-1/
+
+Link to repository: https://github.com/Samualjensen/class-challenge-1
+
+## Installation
+
+Upload index.html, license.txt, and assets folder to the deployed webserver. The develop folder in the repository contains images for the webage, javascript files, and CSS files. The live url included above was provided using GitHub Pages.
+
+## Webapge Image
+
+![img](./Develop/images/js-password-generator-screenshot.png)
+
+## Credits 
+
+Starter code was provided by Xandromus through Github and given to us though the Bootcamp spot. Some additional guidence and help was provided by the #askbcs learning assistant through slack.
+
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
